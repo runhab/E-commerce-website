9e4d2f9db2dc990327828f9daf536d5c04172b76
runhab <habfenethio@gmail.com>
2024-03-07 01:02:21 +0300

    add to cart functionality added

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignup = () => {
         </p>
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
-          <p>By continuing , i agree to the terms of use and privacy policy</p>
+          <p>By continuing , I agree to the terms of use and privacy policy</p>
 
         </div>
       </div>
